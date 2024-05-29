6c6ad98c4313d29fad2b3095b6e962418f804205
Shubhajit <shubjitgithub@gmail.com>
2024-05-29 07:25:04 +0530

    Done

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -36,7 +36,12 @@ public class Lab {
         int sum = 0;
         int i = 0;
 
-//        code here
+        //        code here
+        while (i < end) {
+            sum += i;
+            i++;
+    
+}
 
         return sum;
     }
